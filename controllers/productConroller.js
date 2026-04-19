de4f2c0d97cae22fa6306175c026ab71926cfdf5
Palash Patel <palzz2469@gmail.com>
2026-04-20 00:16:10 +0530

    fix: vite url

--- controllers/productConroller.js
+++ controllers/productConroller.js
@@ -41,7 +41,7 @@ export const createProductController = async (req, res) => {
       shipping,
       category,
       slug: slugify(name),
-      photo: photo.path, // or photo.filename depending on your storage
+      photo: photo, // or photo.filename depending on your storage
     });
 
     await product.save();
