c89f5b305cd0c9a7e8f1315f85ed7d34f26a3435
Palash Patel <palzz2469@gmail.com>
2026-04-20 01:01:51 +0530

    fix: vite url

--- frontend/src/pages/Admin/CreateProduct.jsx
+++ frontend/src/pages/Admin/CreateProduct.jsx
@@ -6,7 +6,6 @@ import { toast } from "react-toastify";
 import axios from "axios";
 import { Button } from "antd";
 import { useNavigate } from "react-router-dom";
-import { set } from "mongoose";
 const { Option } = Select;
 
 const CreateProduct = () => {
