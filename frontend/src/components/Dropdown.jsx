d6e75ab7310fd8fbe748c39b1bfe72fe0e7bbe3e
Palash Patel <palzz2469@gmail.com>
2026-04-19 20:51:21 +0530

    fix: vite url

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -31,7 +31,7 @@ export default function Example() {
           
           <MenuItem>
             <NavLink 
-              to={`/dashboard/${auth?.user?.role === 1 ? 'admin' : 'user'}`}
+              to={`/dashboard/admin`}
                className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
             >
               Dashboard
