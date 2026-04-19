65f59c002a6f5fad4af6cd57fabab552570c49a5
Palash Patel <palzz2469@gmail.com>
2026-04-19 22:34:51 +0530

    fix: vite url

--- frontend/src/pages/user/Profile.jsx
+++ frontend/src/pages/user/Profile.jsx
@@ -3,11 +3,16 @@ import Layout from '../../components/Layout/Layout';
 import UserMenu from '../../components/Layout/UserMenu';
 
 export default function Profile() {
+    const [auth, setAuth] = useAuth();
   return (
     <Layout title={'Dashboard - Orders'}>
         <div className=' grid grid-cols-4 mr-5'>
             <div className='p-5 col-span-1 text-xl border-r-5 border-zinc-300'>
                 <UserMenu></UserMenu>
+            </div>
+            <div className='p-5 col-span-3'>
+                <h1>Profile</h1>
+                <p>Welcome, {auth.user?.name}!</p>
             </div>
         </div>
     </Layout>
