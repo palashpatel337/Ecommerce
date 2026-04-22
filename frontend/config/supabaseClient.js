0ff32e4ae49daa3a2f4989e75347fafe7a7808d5
Palash Patel <palzz2469@gmail.com>
2026-04-22 12:23:45 +0530

    fix:

--- frontend/config/supabaseClient.js
+++ frontend/config/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from ("@supabase/supabase-js");
+import { createClient } from "@supabase/supabase-js";
 
 const { VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY } = import.meta.env;
 
