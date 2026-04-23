69d3a8faac7d8706cf22c3bd0c6e694552375bfc
Palash Patel <palzz2469@gmail.com>
2026-04-24 01:07:27 +0530

    fix: integrating payment (razorpay) options

--- frontend/src/pages/AuthDashboard.jsx
+++ frontend/src/pages/AuthDashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../config/supabaseClient";
+import { supabase } from "../../config/supabaseClient";
 
 export default function Dashboard() {
   const [user, setUser] = useState(null);
